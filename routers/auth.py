4592d0e34ec6c5db01cd470cd8c1b6ff204714c2
SilverBullet19 <a_gouasmia@estin.dz>
2023-12-07 11:55:53 +0100

    added login

--- routers/auth.py
+++ routers/auth.py
@@ -1,15 +1,22 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from config.db import get_db
 from sqlalchemy.orm import Session
-from schemas.auth import UserRegisterSchema
+from schemas.auth import UserRegisterSchema, LoginSchema
 from repositories import user
 from utils.jwt import JWT
+from utils.hashing import Hash
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 @router.post("/login")
-def login():
-    return {"message": "login"}
+def login(loginSchema: LoginSchema, db: Session = Depends(get_db)):
+    userExists = user.get_by_email(db, loginSchema.email)
+    if not userExists:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    if not Hash.compare(userExists.password, loginSchema.password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect password")
+    token = JWT.create_token({"id": userExists.id, "email": userExists.email})
+    return {"token": token}
 
 @router.post("/register-user")
 def register_user(userRegisterSchema: UserRegisterSchema, db: Session = Depends(get_db)):
--- schemas/auth.py
+++ schemas/auth.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 
-class UserRegisterSchema(BaseModel):
-    nom: str
-    prenom: str
+class LoginSchema(BaseModel):
     email: str
     password: str
+
+class UserRegisterSchema(LoginSchema):
+    nom: str
+    prenom: str
