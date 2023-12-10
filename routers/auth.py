31d81dfdf1dc1fc0f661e7fe73f28afb222c5c1b
SilverBullet19 <a_gouasmia@estin.dz>
2023-12-10 21:01:13 +0100

    added avocat entity and setup register avocat

--- routers/auth.py
+++ routers/auth.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from config.db import get_db
 from sqlalchemy.orm import Session
-from schemas.auth import UserRegisterSchema, LoginSchema
-from repositories import user
+from schemas.auth import UserRegisterSchema, LoginSchema, AvocatRegisterSchema
+from repositories import user, avocat
 from utils.jwt import JWT
 from utils.hashing import Hash
 
@@ -24,5 +24,15 @@ def register_user(userRegisterSchema: UserRegisterSchema, db: Session = Depends(
     if userExists:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already used")
     newUser = user.create(db, userRegisterSchema)
+    token = JWT.create_token({"id": newUser.id, "email": newUser.email})
+    return {"token": token}
+
+@router.post("/register-avocat")
+def register_avocat(avocatRegisterSchema: AvocatRegisterSchema, db: Session = Depends(get_db)):
+    userExists = user.get_by_email(db, avocatRegisterSchema.email)
+    if userExists:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already used")
+    newUser = user.create(db, avocatRegisterSchema)
+    avocat.create(db, {"userId": newUser.id, **avocatRegisterSchema.model_dump()})
     token = JWT.create_token({"id": newUser.id, "email": newUser.email})
     return {"token": token}
--- schemas/auth.py
+++ schemas/auth.py
@@ -7,3 +7,18 @@ class LoginSchema(BaseModel):
 class UserRegisterSchema(LoginSchema):
     nom: str
     prenom: str
+
+class AvocatRegisterSchema(UserRegisterSchema):
+    address: str
+    phoneNumber: str
+    facebookUrl: str
+    description: str
+    categories: list[str] = []
+
+class CreateAvocatSchema(BaseModel):
+    address: str
+    phoneNumber: str
+    facebookUrl: str
+    description: str
+    categories: list[str] = []
+    userId: int
