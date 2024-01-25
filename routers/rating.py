04c013fe6e2c42b2a3e05c67069d93c6f79332ef
ZermatAbdenour <a_zermat@estin.dz>
2024-01-25 01:16:51 +0100

    Secure rating

--- routers/rating.py
+++ routers/rating.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException,status
+from fastapi import APIRouter, Depends, HTTPException, Request,status
+from fastapi.security import HTTPBearer
 from sqlalchemy.orm import Session
 from config.db import get_db
 from repositories.rating import aleadyRated, rateAvocat,getAvocatRatingsAndComments
@@ -8,19 +9,20 @@ from repositories import avocat,user
 
 router = APIRouter(prefix="/rating", tags=["Rating"])
 
+bearer_scheme = HTTPBearer()
 
 @router.post("/rate")
-def rate(rateSchema: RateSchema,db:Session=Depends(get_db)):
+def rate(request:Request,rateSchema: RateSchema, token: str = Depends(bearer_scheme),db:Session=Depends(get_db)):
     #check if the user and the avocat exist
     checkavocat = avocat.get_by_id(db, rateSchema.avocatId)
-    checkuser = user.get_by_id(db, rateSchema.userId)
+    checkuser = user.get_by_id(db, request.state.user["id"])
     if (not checkavocat or not checkuser):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User or Avocat not found")
     #check if the user is rating his self
     if checkuser.id == checkavocat.userId:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You can't rate your self")
     #check if the user already rated this avocat
-    if(aleadyRated(rateSchema.avocatId,rateSchema.userId,db)):
+    if(aleadyRated(rateSchema.avocatId,request.state.user["id"],db)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already rated this avocat")
     #validate the rate
     if(rateSchema.rate < 0 or rateSchema.rate > 5):
