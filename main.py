a194bba8c74cba24bfd08b5c8c8a369c11b06b3f
ahmed gouasmia <a_gouasmia@estin.dz>
2024-01-26 15:58:42 +0100

    fixed cors

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST", "GET", "PUT", "DELETE"],
+    allow_methods=["POST", "GET", "PUT", "PATCH", "DELETE"],
     allow_headers=["*"],
 )
 
